3e7ad6389133b27ddff9a8d63775fc89b7789474
Samuel Bennion <bennionsamuel@gmail.com>
2019-10-18 12:40:24 -0600

    Add clear board functionality

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ function createDivs(numOfDivs){
         container.appendChild(cell);
     }
 }
+function clearStyling(){
+    cells.forEach((cell) => {
+        cell.classList.remove('hovered');
+        cell.classList.add('notHovered');
+    });
+}
 const container = document.querySelector('#container');
-
+const clearBoard = document.getElementById('clear');
 createDivs(16);
 const cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', function(){
+        cell.classList.remove('notHovered');
         cell.classList.add('hovered');
     });
+});
+clearBoard.addEventListener('click', function(e){
+    clearStyling();
 });
